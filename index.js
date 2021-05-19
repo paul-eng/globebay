629cb5f9661ef21f20d4837d447751ab3471fe72
Paul <pje224@nyu.edu>
2021-05-19 02:46:33 -0400

    Server forwards to frontend at root directory

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const ebay = require("./routes/ebay");
+const path = require("path");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use("/ebay", ebay);
 
-app.use("/ebay",ebay)
+// Heroku specific HTTPS redirect solution c/o Jake Trent
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
+
+app.use(express.static(__dirname));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
