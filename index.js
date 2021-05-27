e3807bd7f8c9320e4b38a9377f29d27940ac797b
Paul <pje224@nyu.edu>
2021-05-27 19:00:13 -0400

    Add heroku-keep-awake

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const ebay = require("./routes/ebay");
 const google = require("./routes/google");
 const path = require("path");
 const app = express();
+const { wakeDyno } = require("heroku-keep-awake");
 app.use(cors());
 app.use(express.json());
 app.use("/ebay", ebay);
 app.use("/google", google);
-
 
 // Heroku specific HTTPS redirect solution c/o Jake Trent
 if (process.env.NODE_ENV === "production") {
@@ -24,5 +24,15 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
+const DYNO_URL = "https://globebay.herokuapp.com/";
+
+// const opts = {
+//   logging: false,
+//   stopTimes: { start: "08:30", end: "19:15" },
+// };
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  wakeDyno(DYNO_URL);
+  console.log(`Server running on port ${port}`);
+});
