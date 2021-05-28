fd8da67168c6bd57b2f9276508e21e34b47a7750
Paul <pje224@nyu.edu>
2021-05-28 11:21:56 -0400

    Disable force SSL for custom domain

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const ebay = require("./routes/ebay");
 const google = require("./routes/google");
 const path = require("path");
 const app = express();
-const pingCustom = require("./ping-custom");
-const url = "https://globebay.herokuapp.com/";
 
 app.use(cors());
 app.use(express.json());
 app.use("/ebay", ebay);
 app.use("/google", google);
 
-// Heroku specific HTTPS redirect solution c/o Jake Trent
-if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    if (req.header("x-forwarded-proto") !== "https")
-      res.redirect(`https://${req.header("host")}${req.url}`);
-    else next();
-  });
-}
+// Heroku specific force HTTPS redirect solution c/o Jake Trent (Cannot be used w custom domains at free tier)
+// if (process.env.NODE_ENV === "production") {
+//   app.use((req, res, next) => {
+//     if (req.header("x-forwarded-proto") !== "https")
+//       res.redirect(`https://${req.header("host")}${req.url}`);
+//     else next();
+//   });
+// }
 
 app.use(express.static(__dirname));
 app.get("*", (req, res) => {
@@ -28,6 +26,5 @@ app.get("*", (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  pingCustom(url);
   console.log(`Server running on port ${port}`);
 });
