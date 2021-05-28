150101a96b8636b8d45d1305e7d6ac2d2f97b895
Paul <pje224@nyu.edu>
2021-05-27 22:02:56 -0400

    Try diff package

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ebay = require("./routes/ebay");
 const google = require("./routes/google");
 const path = require("path");
 const app = express();
-const { wakeDyno } = require("heroku-keep-awake");
+
 app.use(cors());
 app.use(express.json());
 app.use("/ebay", ebay);
@@ -24,15 +24,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-const DYNO_URL = "https://globebay.herokuapp.com/";
-
-// const opts = {
-//   logging: false,
-//   stopTimes: { start: "08:30", end: "19:15" },
-// };
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  wakeDyno(DYNO_URL);
   console.log(`Server running on port ${port}`);
 });
